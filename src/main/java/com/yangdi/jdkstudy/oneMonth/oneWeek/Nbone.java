35a04725bf6753fe433ebca4b88948d260ba09a5
huwenjian <hwj_gz@163.com>
2018-08-11 23:41:59 +0800

    yangdi : test1 计算int 和

---
+++ src/main/java/com/yangdi/jdkstudy/oneMonth/oneWeek/Nbone.java
@@ -0,0 +1,18 @@
+package com.yangdi.jdkstudy.oneMonth.oneWeek;
+
+public class Nbone {
+
+
+    //计算出1+2+3+4+5 = 15
+
+    public static void main(String[] args) {
+        int wo = 1;
+        int shi = 2;
+        int yang = 3;
+        int di = 4;
+        int a = 5;
+        System.out.print(wo + shi + yang + di + a + "换行\n");
+        System.out.print(wo + shi + yang + di + a + "换行\"\"\"\"\"\"\"\"\n");
+                System.out.println(wo + shi + yang + di + a);
+    }
+}
