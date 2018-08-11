083a5b9f2770bbccca9d608bcc112cf5e38b89ad
huwenjian <hwj_gz@163.com>
2018-08-11 23:05:20 +0800

    yangdi init project

---
+++ src/main/java/com/yangdi/jdkstudy/JdkstudyApplication.java
@@ -0,0 +1,12 @@
+package com.yangdi.jdkstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdkstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdkstudyApplication.class, args);
+    }
+}
